e20aed26f16ac51a0964c826edcccabd36a5249f
Matheus <oliveira10mais@hotmail.com>
2026-03-12 21:22:36 -0300

    first commit

---
+++ src/test/java/com/vb/produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vb.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
